ba479bd7f2696a86bb69adee0d9beeb4761a8900
justtoin <justychebrys@gmail.com>
2021-07-09 00:37:31 +0200

    Included OhScic and mfMhoc classes to question box detection

--- similarQuestionBoxRemover.js
+++ similarQuestionBoxRemover.js
@@ -5,7 +5,7 @@ chrome.storage.sync.get(["active"], (res) => {
             var h2tags = document.getElementsByTagName("h2");
         
             Array.from(h2tags).forEach((el) => {
-                if(el.classList.contains("JolIg")) {
+                if(el.classList.contains("JolIg") &&  el.classList.contains("OhScic") && el.classList.contains("mfMhoc")) {
                     //Check if previous element in html structure has 'hlcw0c' class (larger free space after it)
                     
                     var questionBox = el.parentElement.parentElement.parentElement.parentElement.parentElement;
