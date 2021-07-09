d103ed459cf632b40474036a9160d31e259649eb
jandestiny <39858598+jandestiny@users.noreply.github.com>
2021-07-09 11:47:10 +0200

    - removed 1 parentelement -> issue #1 fixed

--- similarQuestionBoxRemover.js
+++ similarQuestionBoxRemover.js
@@ -4,11 +4,11 @@ chrome.storage.sync.get(["active"], (res) => {
             //Scan if text of h2 tags contains 'Ähnliche Fragen'
             var h2tags = document.getElementsByTagName("h2");
         
-            Array.from(h2tags).forEach((el) => {
-                if(el.classList.contains("JolIg") &&  el.classList.contains("OhScic") && el.classList.contains("mfMhoc")) {
+            Array.from(h2tags).forEach((h2Tag) => {
+                if(h2Tag.classList.contains("JolIg") &&  h2Tag.classList.contains("OhScic") && h2Tag.classList.contains("mfMhoc")) {
                     //Check if previous element in html structure has 'hlcw0c' class (larger free space after it)
                     
-                    var questionBox = el.parentElement.parentElement.parentElement.parentElement.parentElement;
+                    var questionBox = h2Tag.parentElement.parentElement.parentElement.parentElement;
                     var previousElement = questionBox.previousElementSibling;
         
                     if(previousElement && previousElement.classList.contains("hlcw0c")) {
