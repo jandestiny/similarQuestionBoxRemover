afacf59e2593e97220f0812b723c8033c38516d2
jandestiny <39858598+jandestiny@users.noreply.github.com>
2021-07-09 12:30:37 +0200

    - rewrote code to be more efficient and compact

--- similarQuestionBoxRemover.js
+++ similarQuestionBoxRemover.js
@@ -1,24 +1,17 @@
 chrome.storage.sync.get(["active"], (res) => {
-    if(res.active) {
+    if (res.active) {
         document.body.onload = () => {
-            //Scan if text of h2 tags contains 'Ähnliche Fragen'
-            var h2tags = document.getElementsByTagName("h2");
-        
-            Array.from(h2tags).forEach((h2Tag) => {
-                if(h2Tag.classList.contains("JolIg") &&  h2Tag.classList.contains("OhScic") && h2Tag.classList.contains("mfMhoc")) {
-                    //Check if previous element in html structure has 'hlcw0c' class (larger free space after it)
-                    
-                    var questionBox = h2Tag.parentElement.parentElement.parentElement.parentElement;
-                    var previousElement = questionBox.previousElementSibling;
-        
-                    if(previousElement && previousElement.classList.contains("hlcw0c")) {
-                        previousElement.classList.remove("hlcw0c");
-                        previousElement.classList.add("g");
-                    }
-                    
-                    questionBox.remove();
+            let box = document.querySelector(".related-question-pair")
+
+            if (box)
+            {
+                for (let i = 0; i < 5; i++)
+                {
+                    box = box.parentElement
                 }
-            });
+
+                box.innerHTML = ""
+            } 
         }
     }
 });
